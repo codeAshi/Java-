d1b6d1ad60d7391d3db672ba7d7b8970b2a4cfd3
Ashish Bedare <ashishbedare9@gmail.com>
2026-07-15 20:12:02 +0530

    Permutation Algorithm

---
+++ permutationArray.java
@@ -0,0 +1,24 @@
+public class permutationArray {
+
+
+    void permu(int[] nums,ArrayList<Integer> res,int idx){
+
+        if(i==nums.length){
+            System.out.println(res);
+            return;
+        }
+        for(int i=)
+        int temp=nums[i];
+
+    }
+    public static void main(String[] args){
+        int[] nums={1,2,3};
+        ArrayList<Integer> res=new ArrayList<>();
+        permutationArray obj = new permutationArray();
+        obj.permu(nums,res,0);
+
+
+
+    }
+    
+}
